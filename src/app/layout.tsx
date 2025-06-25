25c3e64b11f5956a92d3fc4b8ef0d7f963f82e13
javeri-a <javeriaqureshi746@gmail.com>
2025-06-25 10:22:30 +0500

    add cursor.tsx file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/page";
+import SplashCursor from './SplashCursor';
+
+
 
 
 const geistSans = Geist({
@@ -33,6 +36,8 @@ export default function RootLayout({
         
         
         {children}
+        
+<SplashCursor />
       </body>
     </html>
   );
